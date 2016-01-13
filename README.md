e3f339ce0c174b58af72476e430f58be4b9f4015
vingaike <vingaike@gmail.com>
2016-01-13 17:47:26 +0000

    swagger added

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# spring-boot-template
- Template for Spring Boot
+# spring-boot-microservice
+ MicroService built using Spring Boot Framework
+
+Build
+----------------------------
+mvn clean package
+
+Run Application
+----------------------------
+java -jar target/spring-boot-microservice-1.0-SNAPSHOT.jar
+
+Test Endpoint
+----------------------------
+http://localhost:8080/hello
+http://localhost:8080/greeting
+
+Swagger Endpoint
+----------------------------
+http://localhost:8080/api-docs
---
+++ src/main/java/com/sample/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.sample.config;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
+import com.wordnik.swagger.model.ApiInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableSwagger
+public class SwaggerConfig {
+    private SpringSwaggerConfig springSwaggerConfig;
+
+    @Autowired
+    public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+        this.springSwaggerConfig = springSwaggerConfig;
+    }
+
+    @Bean
+    public SwaggerSpringMvcPlugin customImplementation() {
+        return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
+                apiInfo()).includePatterns("/.*");
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo("Spring Boot", "Spring Boot Microservice",
+                "Spring Boot Microservice terms of service", "example@gmail.com",
+                "Spring Boot Microservice Licence Type", "Spring Boot Microservice License URL");
+        return apiInfo;
+    }
+}
